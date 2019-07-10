ca27a9cc887edaab5e447df2748459e8d8cc6b33
Stanislav Gaistruk <stanislas.gaistruk@amadeus.com>
2019-07-10 12:46:03 +0200

    improved demo data

--- src/main/resources/sample-tickets.sql
+++ src/main/resources/sample-tickets.sql
@@ -1,16 +1,5 @@
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (1, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (2, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (3, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (4, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (5, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (6, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (7, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
-INSERT INTO ticket (id, summary, description, email, status, priority)
-    VALUES (8, 'No electricity', 'No electricity after the storm on yesterday', 'email@example.com', 1, 1);
+INSERT INTO ticket (id, created, summary, description, email, status, priority)
+    VALUES  (1, '2019-07-06', 'No electricity', 'No electricity in Bludhaven after the storm on yesterday', 'jimmy@example.com', 1, 4),
+            (2, '2019-07-07', 'Birds messing with wires', 'Three unidentified birds seen messing with electrical wires and causing outages in Duckburg.', 'ruth@example.com', 2, 2),
+            (7, '2019-07-08', 'Azed5 button not working', 'Smallville power plant having issues after tests. Engineer tried Azed5 button but still broken.', 'george@example.com', 2, 5),
+            (8, '2019-07-09', 'Disabled ticket', 'This ticket should not be visible', 'email@example.com', 3, 1);
